5c7d59909ce62d5cdfedb825d7e71ac6edb8b837
Artem <atremat@gmail.com>
2024-06-10 01:57:54 +0300

    step 1

---
+++ src/controllers/contacts.js
@@ -0,0 +1,24 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+import createHttpError from 'http-errors';
+
+export const getAllContactsController = async () => {
+  const contacts = await getAllContacts();
+  return contacts;
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+
+  //if contact is not found, then 404
+  if (!contact) {
+    next(createHttpError(404, `Not found contact with id ${contactId}!`));
+    return;
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import productsRouter from './routers/contacts.js';
 
 const PORT = Number(env('PORT', 3000));
 
@@ -20,35 +20,7 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    console.log('contacts: ', contacts);
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    //if contact is not found, then 404
-    if (!contact) {
-      return res.status(404).json({
-        status: 404,
-        message: `Not found contact with id ${contactId}!`,
-      });
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
+  app.use(productsRouter);
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
